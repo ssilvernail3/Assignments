ac9b423d361300634a48faab1a851b228c6e660d
shane <shanesilvernail3@gmail.com>
2021-06-02 11:34:06 -0400

    finished let and const exercise

---
+++ LetAndConstExercise/letAndConst.js
@@ -0,0 +1,12 @@
+//1
+
+var PI = 3.14;
+PI = 42;
+//You would need to change this to 
+const PI = 3.14;
+
+//QUIZ
+//The difference between var and let is the scoping of var which is functionally scopped compared to let which is block scoped. Also let can only be reassigned but not redeclared. 
+//The difference between var and const is also the scopping and the reassingment of variables. Var can be reassigned and redeclared whereas const can do neither. 
+//The difference between let and const is the variable properites, let can be reassigned where const cannot.
+//Hoisting is the property that allows variables declared with var to return as undefined before initialization whereas variables declared with let or const need to be initialized or else an error is thrown in the console. 
