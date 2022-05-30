1269b3f0a8c399c544975fcc387b465867c133b6
Shane Silvernail <shanesilvernail3@gmail.com>
2022-05-29 21:39:54 -0400

    finished messagely

---
+++ Express/BCrypt/Messagely/models/user.js
@@ -0,0 +1,172 @@
+/** User class for message.ly */
+
+const res = require("express/lib/response");
+const { user } = require("pg/lib/defaults");
+const { DB_URI, BCRYPT_WORK_FACTOR } = require("../config");
+const db = require("../db");
+const ExpressError = require("../expressError");
+const bcrypt = require('bcrypt'); 
+
+
+
+/** User of the site. */
+
+class User {
+
+
+  // constructor({ username, password, first_name, last_name, phone }) {
+  //   this.username = username
+  //   this.password = password
+  //   this.first_name = first_name
+  //   this.last_name = last_name
+  //   this.phone = phone 
+      
+  // }
+
+  /** register new user -- returns
+   *    {username, password, first_name, last_name, phone}
+   */
+
+
+  static async register({ username, password, first_name, last_name, phone }) { 
+    let hashedPw = await bcrypt.hash(password, BCRYPT_WORK_FACTOR); 
+    const result = await db.query(`INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at) VALUES ($1, $2, $3, $4, $5) RETURNING username, password, first_name, last_name, phone, current_timestamp, current_timestamp`, [username, hashedPw, first_name, last_name, phone])
+    return res.json(result.rows[0]); 
+  }
+
+  /** Authenticate: is this username/password valid? Returns boolean. */
+
+  static async authenticate(username, password) { 
+    const result = await db.query(
+      "SELECT password FROM users WHERE username = $1",
+      [username]);
+  let user = result.rows[0];
+
+  return user && await bcrypt.compare(password, user.password);
+}
+  
+
+  /** Update last_login_at for user */
+
+  static async updateLoginTimestamp(username) {
+    const result = await db.query(
+      `UPDATE users
+         SET last_login_at = current_timestamp
+         WHERE username = $1
+         RETURNING username`,
+      [username]);
+
+  if (!result.rows[0]) {
+    throw new ExpressError(`No such user: ${username}`, 404);
+  }
+}
+   
+
+  /** All: basic info on all users:
+   * [{username, first_name, last_name, phone}, ...] */
+
+  static async all() { 
+    
+      const results = await db.query('SELECT username, first_name, last_name, phone FROM users');
+      return results.rows.map(u => new User(u)); 
+   
+  }
+
+  /** Get: get user by username
+   *
+   * returns {username,
+   *          first_name,
+   *          last_name,
+   *          phone,
+   *          join_at,
+   *          last_login_at } */
+
+  
+
+  static async get(username) { 
+    const result = await db.query(`SELECT first_name, last_name, phone, join_at, last_login_at FROM users WHERE username = $1 RETURNING username, first_name, last_name, phone, join_at, last_login_at`, [username]);
+    const user = result.rows[0];
+    if (user === undefined) {
+      throw new ExpressError(`Username: ${username} does not exist`, 404)
+    }
+    return new User(user); 
+  }
+
+  /** Return messages from this user.
+   *
+   * [{id, to_user, body, sent_at, read_at}]
+   *
+   * where to_user is
+   *   {username, first_name, last_name, phone}
+   */
+
+  static async messagesFrom(username) { 
+    const result = await db.query(
+      `SELECT m.id,
+              m.to_username,
+              u.first_name,
+              u.last_name,
+              u.phone,
+              m.body,
+              m.sent_at,
+              m.read_at
+        FROM messages AS m
+          JOIN users AS u ON m.to_username = u.username
+        WHERE from_username = $1`,
+      [username]);
+
+  return result.rows.map(m => ({
+    id: m.id,
+    to_user: {
+      username: m.to_username,
+      first_name: m.first_name,
+      last_name: m.last_name,
+      phone: m.phone
+    },
+    body: m.body,
+    sent_at: m.sent_at,
+    read_at: m.read_at
+  }));
+  }
+
+  /** Return messages to this user.
+   *
+   * [{id, from_user, body, sent_at, read_at}]
+   *
+   * where from_user is
+   *   {username, first_name, last_name, phone}
+   */
+
+  static async messagesTo(username) { 
+    const result = await db.query(
+      `SELECT m.id,
+              m.from_username,
+              u.first_name,
+              u.last_name,
+              u.phone,
+              m.body,
+              m.sent_at,
+              m.read_at
+        FROM messages AS m
+         JOIN users AS u ON m.from_username = u.username
+        WHERE to_username = $1`,
+      [username]);
+
+  return result.rows.map(m => ({
+    id: m.id,
+    from_user: {
+      username: m.from_username,
+      first_name: m.first_name,
+      last_name: m.last_name,
+      phone: m.phone,
+    },
+    body: m.body,
+    sent_at: m.sent_at,
+    read_at: m.read_at
+  }));
+}
+  
+}
+
+
+module.exports = User;
