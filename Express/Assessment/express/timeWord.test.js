895a213d485ef3ae676a21b5584c412ebf15ab52
Shane Silvernail <shanesilvernail3@gmail.com>
2022-06-08 21:43:37 -0400

    updated file

--- Express/Assessment/express/timeWord.test.js
+++ Express/Assessment/express/timeWord.test.js
@@ -8,22 +8,22 @@ describe('#timeword', () => {
 
 describe('test noon', () => {
   test('to see if it is noon', () => {
-    expect(timeWord(12, '00').toEqual('noon'));
+    expect(timeWord(12, 00).toEqual('noon'));
   });
 });
 describe('test midnight', () => {
   test('to see if it is midnight', () => {
-    expect(timeWord('00', '00').toEqual('midnight'));
+    expect(timeWord(00, 00).toEqual('midnight'));
   });
 });
 describe('test 1PM', () => {
   test('to see if it is 1 o`clock pm', () => {
-    expect(timeWord(13, '00').toEqual('one o`clock pm'));
+    expect(timeWord(13, 00).toEqual('one o`clock pm'));
   });
 });
 describe('doesnt workd', () => {
   test('to see if nums outisde of range', () => {
-    expect(timeWord(44, '00').toEqual(undefined));
+    expect(timeWord(44, 00).toEqual(undefined));
   });
 });
 
